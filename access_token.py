5f98ace763c7ff180b9b766cec8f5711e8279693
kooqooo <koogmlcks@gmail.com>
2024-06-12 19:27:47 +0900

    stye: black formatter 적용

--- access_token.py
+++ access_token.py
@@ -1,21 +1,14 @@
 import base64
 import requests
-import yaml
-
-# Load secrets
-with open('secrets.yaml') as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
-    
-url = config['url'] + '/v1/tokes'
-client_id = config['client_id'] 
-client_secret = config['client_secret']
 
 
 def base64_encode(string: str) -> str:
-    return base64.b64encode(string.encode('utf-8')).decode('utf-8')
+    return base64.b64encode(string.encode("utf-8")).decode("utf-8")
+
 
 def base64_decode(string: str) -> str:
-    return base64.b64decode(string.encode('utf-8')).decode('utf-8')
+    return base64.b64decode(string.encode("utf-8")).decode("utf-8")
+
 
 # 1. Header 방식
 """ cURL 예시
@@ -24,15 +17,18 @@ url --location url + '/v1/tokens'
     --header 'Grant-Type: client_credentials' 
     --header 'Content-Type: application/json' 
 """
+
+
 def get_response_by_header(client_id: str, client_secret: str) -> str:
-    encoded = base64_encode(f'{client_id}:{client_secret}')
+    encoded = base64_encode(f"{client_id}:{client_secret}")
     headers = {
-        'Authorization': f'Basic {encoded}',
-        'Grant-Type': 'client_credentials',
-        'Content-Type': 'application/json'
-        }
+        "Authorization": f"Basic {encoded}",
+        "Grant-Type": "client_credentials",
+        "Content-Type": "application/json",
+    }
     response = requests.post(url=url, headers=headers)
     return response.json()
+
 
 # 2. -u 옵션 방식
 """
@@ -40,7 +36,15 @@ curl --location url + '/v1/tokens'
       -u <Client ID:ClientSecret> --basic 
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import yaml
+
+    with open("secrets.yaml", encoding="utf-8") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    url = config["url"] + "/v1/tokes"
+    client_id = config["client_id"]
+    client_secret = config["client_secret"]
+
     response = get_response_by_header(client_id, client_secret)
-    print(response)    
-    
+    print(response)

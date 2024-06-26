c4e3ae71e200125ca71f4934c7ecf60a7979c172
kooqooo <koogmlcks@gmail.com>
2024-06-26 16:12:18 +0900

    feat: chroma db를 사용하여 유사도 문장 추출
    #4

--- vectordb_manager.py
+++ vectordb_manager.py
@@ -1,60 +1,88 @@
+from pathlib import Path
+
+import torch
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface.embeddings.huggingface import HuggingFaceEmbeddings
 
+from text_splitters import character_splitter
 
-def faiss_inference(query):
-    """
-    faiss를 이용해 JD 키워드 한줄한줄 받아와서 유사한 기술면접 질문 4개씩을 불러옵니다.
-    output으로는 (4 * 키워드 줄 개수 - 중복된 문장)개의 질문이 반환됩니다
-    """
-    embeddings = HuggingFaceEmbeddings(model_name="jhgan/ko-sroberta-multitask", model_kwargs={"device": "cuda"})
-    store_name = "./FAISS_INDEX_TAG"
-    new_db = FAISS.load_local(store_name, embeddings, allow_dangerous_deserialization=True)
-    results = query.split("\n")
-    final_result = []
-    for i in results:
-        docs = new_db.similarity_search(i, k=4)
-        page_contents = [final_result.append(doc.page_content) for doc in docs]
-    # 중복된 문장 제거
-    final_result = list(set(final_result))
-    return final_result
+# Global variables
+model_name = "jhgan/ko-sroberta-multitask"
+model_kwargs = {'device': "cuda" if torch.cuda.is_available() else "cpu"}
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+embeddings = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+faiss_store_name = "./FAISS_INDEX_TAG"
+
+
+# def make_faiss_index(path: str | Path):
+#     split_docs = TextLoader(path).load_and_split(character_splitter)
+#     new_db = FAISS.from_documents(split_docs, embeddings)
+#     new_db.save_local(faiss_store_name)
+
+
+# def faiss_inference(query):
+#     new_db = FAISS.load_local(faiss_store_name, embeddings, allow_dangerous_deserialization=True)
+#     results = query.split("\n")
+#     final_result = []
+#     for i in results:
+#         docs = new_db.similarity_search(i, k=4)
+#         page_contents = [final_result.append(doc.page_content) for doc in docs]
+#     # 중복된 문장 제거
+#     final_result = list(set(final_result))
+#     return final_result
 
 
 def main():
-    import torch
-    from text_splitters import character_splitter
+    def _print(docs):
+        for idx, doc in enumerate(docs):
+            score = None
+            this = '->' if idx == 0 else '  '
+            if type(doc) == tuple:
+                doc, score = doc
+            print(f"{this} content : {doc.page_content}")
+            print(f"{this} metadata : {doc.metadata}") if doc.metadata else None
+            print(f"{this} score : {score}") if score is not None else None
+            print()
+        print()
 
-    model_name = "jhgan/ko-sroberta-multitask"
-    model_kwargs = {'device': "cuda" if torch.cuda.is_available() else "cpu"}
-    encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
-
-    hf_embeddings = HuggingFaceEmbeddings(
-        model_name=model_name,
-        model_kwargs=model_kwargs,
-        encode_kwargs=encode_kwargs
-    )
-
-    # # TextLoader의 load_and_split 메소드를 사용하는 방식
-    # split_docs = TextLoader("./wiki.txt").load_and_split(character_splitter)
-    # chroma_db = Chroma.from_documents(split_docs, hf_embeddings)
-
+    # TextLoader의 load_and_split 메소드를 사용하는 방식
+    data_path = "./wiki_python.txt"
+    split_docs = TextLoader(data_path).load_and_split(character_splitter) # 파이썬 위키백과 문서
     # # Print the split documents
-    # # print(*[split_doc.page_content for split_doc in split_docs], sep='\n\n')
-    # for idx, split_doc in enumerate(split_docs):
-    #     print(idx, ":", split_doc.page_content, end="\n\n")
+    # print(*[split_doc.page_content for split_doc in split_docs], sep='\n\n')
+    for idx, split_doc in enumerate(split_docs):
+        split_docs[idx].metadata['created_date'] = "1970-01-01"
+        print(idx, ":", split_doc.page_content, end="\n\n")
+    
+    chroma_db = Chroma.from_documents(split_docs, embeddings)
+    # print(f"{type(chroma_db.embeddings) = }")
+    print()
 
-    # 텍스를 직접 넣어서 Chroma 객체를 생성하는 방식
+    query = "파이썬을 어디에서 관리하는가?"
+    print(f"/** '{query}'와 유사도가 높은 답은 '->'로 표시 **/")
+    _print(chroma_db.similarity_search(query))
+    _print(chroma_db.similarity_search_with_score(query))
+    _print(chroma_db.similarity_search_with_relevance_scores(query))
+
+    
+    # 텍스트를 직접 넣어서 Chroma 객체를 생성하는 방식
     texts = ["안녕하세요", "오늘 날씨가 좋네요"]
-    chroma_db = Chroma.from_texts(texts, hf_embeddings)
-    print(f"{chroma_db.embeddings = }")
-    print()
-    print(f"{chroma_db.similarity_search_with_score('반갑습니다', k=2) = }")  # Cosine Distance, 작을수록 유사도가 높음, 0~2 사이의 값
-    print()
-    print(f"{chroma_db.similarity_search_with_relevance_scores('반갑습니다', k=2) = }")  # Relevance Score, 0~1 사이의 값
-    print(f"{chroma_db.similarity_search_with_relevance_scores('반갑습니다', k=2, score_threshold=0.5) = }")
+    chroma_db = Chroma.from_texts(texts, embeddings)
+    # print(f"{chroma_db.embeddings = }")
+    query = "반갑습니다"
+    print(f"/** '{query}'와 유사도가 높은 답은 '->'로 표시 **/")
+    _print(chroma_db.similarity_search_with_score(query, k=2))  # Cosine Distance, 0~2 사이의 값, 작을수록 유사도가 높음
+    _print(chroma_db.similarity_search_with_relevance_scores(query, k=2))  # Relevance Score, 0~1 사이의 값, 클수록 유사도가 높음
+    _print(chroma_db.similarity_search_with_relevance_scores(query, k=2, score_threshold=0.5))
+
+    # # 텍스트를 직접 넣어서 FAISS 객체를 생성하는 방식
 
 
 if __name__ == "__main__":

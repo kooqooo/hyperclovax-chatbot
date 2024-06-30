5000c74848a407d3831e0e94ba7299ea8e6637da
kooqooo <koogmlcks@gmail.com>
2024-06-30 18:14:38 +0900

    refac: 직접만든 프롬프트 템플릿 클래스 사용하기
    #8

--- chat_completions_with_rag.py
+++ chat_completions_with_rag.py
@@ -1,26 +1,21 @@
 from chat_completions import CompletionExecutor, RequestData, parse_response
 from vectordb_manager import faiss_inference
+from prompt_template import Prompts
+
 
 def chat_with_rag(query: str, executor: CompletionExecutor, k: int = 1) -> str:
     retrieval_reulst = faiss_inference(query, k)
     
-    preset_texts = [
-        {"role": "system", "content": "- 너의 역할은 사용자의 질문에 reference를 바탕으로 답변하는거야. \n- 너가 가지고있는 지식은 모두 배제하고, 주어진 reference의 내용만을 바탕으로 답변해야해. \n- 만약 사용자의 질문이 reference와 관련이 없다면, {제가 가지고 있는 정보로는 답변할 수 없습니다.}라고만 반드시 말해야해."}
-    ]
-    
+    text = "- 너의 역할은 사용자의 질문에 reference를 바탕으로 답변하는거야. \n- 너가 가지고있는 지식은 모두 배제하고, 주어진 reference의 내용만을 바탕으로 답변해야해. \n- 만약 사용자의 질문이 reference와 관련이 없다면, {제가 가지고 있는 정보로는 답변할 수 없습니다.}라고만 반드시 말해야해."
+    preset_prompts = Prompts.from_message("system", text)
     for retrieved in retrieval_reulst:
-        preset_texts.append(
-            {
-                "role": "system",
-                "content": f"reference: {retrieved}"
-            }
-        )
-    preset_texts.append({"role": "user", "content": query})
+        preset_prompts.add_message("system", f"reference: {retrieved}")
+    preset_prompts.add_message("user", query)
 
-    request_data = RequestData(messages=preset_texts).to_dict()
-    request_data = executor.execute(request_data)
+    request_data = RequestData(messages=preset_prompts.to_dict()).to_dict()
+    result = executor.execute(request_data)
     
-    return parse_response(request_data)
+    return parse_response(result)
 
 
 if __name__ == "__main__":

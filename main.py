2ca4fbcbc6aa6621295fdbc0eff80c9a298ee08f
kooqooo <koogmlcks@gmail.com>
2024-07-04 04:28:29 +0900

    fix: 테스트 후 버그 수정중...
    #13

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from gridfs import GridFSBucket
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 
 from backend.meetings import router as meeting_router
 from backend.meetings import Attendee, Meeting
-from backend.mongo_config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME
+from backend.mongo_config import *
 from backend.ip_addresses import get_public_ip, get_private_ip
-from stt_inference import transcribe_audio
+from stt_inference import transcribe_audio, atranscribe_audio
 from audio_splitter import asplit_audio
 
 app = FastAPI()
@@ -25,35 +24,37 @@ app.include_router(meeting_router, prefix="/meetings", tags=["meetings"])
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/")
 async def upload_file(file: UploadFile = File(...)):
     client = AsyncIOMotorClient(MONGO_URI)
     db = client[DATABASE_NAME]
     collection = db[COLLECTION_NAME]
-    bucket = GridFSBucket(db)
+    bucket = AsyncIOMotorGridFSBucket(db)
     try:
         # 오디오 파일 분할
         split_files = await asplit_audio(file)
         
         # STT
         transcriptions = []
-        for split_file in split_files:
-            transcription = transcribe_audio(split_file)
+        for split_file in split_files: # 로컬에 저장된 파일을 읽는게 더 빠름, 파일 하나마다 모델을 불러오는 듯?
+            transcription = await atranscribe_audio(split_file) # 비동기로 변경할 필요가 있음
             transcriptions.append(transcription)
         
-         # GridFS에 파일 업로드
+        # GridFS에 파일 업로드
         grid_in = bucket.open_upload_stream(file.filename)
-        await grid_in.write(file.file.read())
+        file_content = await file.read()
+        await grid_in.write(file_content)
         await grid_in.close()
         file_id = grid_in._id
         
         # 회의 정보 저장
         meeting = Meeting(
             title=file.filename,
-            audio_file_id=file_id,
+            audio_file_id=file_id, ####### <- 이 부분이 문제가 될 가능성이 있음
             transcript="\n\n".join(transcriptions)
         )
-        collection.insert_one(meeting)
+        await collection.insert_one(meeting)
         
         return JSONResponse(status_code=200, content={"file_id": str(file_id), "transcriptions": transcriptions})
     except Exception as e:

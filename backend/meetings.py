ac505eea9c35327158272e05c2415dbbc9474415
kooqooo <koogmlcks@gmail.com>
2024-07-04 04:28:11 +0900

    fix: 읽어오는 경로를 추가해야 import가 가능
    - main의 입장에서 이 파일을 보기때문
    
    #13

--- backend/meetings.py
+++ backend/meetings.py
@@ -9,7 +9,7 @@ from pymongo import ReturnDocument, errors
 from motor.motor_asyncio import AsyncIOMotorClient
 import pytz
 
-from mongo_config import *
+from backend.mongo_config import *
 
 from typing import Any, List, Optional
 from pydantic import BaseModel, Field
@@ -26,8 +26,8 @@ class Meeting(BaseModel):
     # date: datetime # 회의 날짜 # 였는데 업로드된 파일의 메타데이터를 읽어오는 것이 어려움
     attendees: Optional[List[Attendee]] = []
     transcript: Optional[str] = None  # 대화 내용을 저장할 필드
-    audio_file_id: Optional[ObjectId] = None  # GridFS에 저장된 음성 파일의 ID
-    faiss_file_id: Optional[ObjectId] = None  # GridFS에 저장된 Faiss 파일의 ID
+    audio_file_id: Optional[str] = None  # GridFS에 저장된 음성 파일의 ID
+    faiss_file_id: Optional[str] = None  # GridFS에 저장된 Faiss 파일의 ID
     # faiss_vector: Optional[List[float]] = None  # Faiss에 저장된 벡터
     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=pytz.timezone('Asia/Seoul'))) # DB에 저장 기준 시간
     # updated_at: datetime = Field(default_factory=datetime.now)

f70b6b99aa5748a5399e9da082e906c790518fc9
kooqooo <koogmlcks@gmail.com>
2024-07-05 02:36:38 +0900

    fix: 추가된 엔드포인트에 맞게 함수 일부 변경
    #13

--- audio_splitter.py
+++ audio_splitter.py
@@ -10,6 +10,9 @@ from file_info_reader import get_file_stats, get_creation_time_from_file_stats, 
 
 
 def split_audio(file_path, min_silence_len=500, silence_thresh=-40, chunk_length=10000, output_dir="output"):
+    filename = os.path.basename(file_path)
+    filename, _ = os.path.splitext(filename)
+    
     # 파일 확장자에 따라 AudioSegment 로드
     audio = AudioSegment.from_file(file_path)
     
@@ -21,6 +24,9 @@ def split_audio(file_path, min_silence_len=500, silence_thresh=-40, chunk_length
         keep_silence=200  # 침묵 구간을 잘라낼 때 약간의 침묵을 유지
     )
     
+    if len(chunks) < 2:
+        return 0
+    
     # 출력 디렉토리 생성
     os.makedirs(output_dir, exist_ok=True)
     
@@ -30,18 +36,18 @@ def split_audio(file_path, min_silence_len=500, silence_thresh=-40, chunk_length
     for chunk in tqdm(chunks):
         current_chunk += chunk
         if len(current_chunk) >= chunk_length:
-            chunk_filename = os.path.join(output_dir, f"part_{part_number}.mp3")
+            chunk_filename = os.path.join(output_dir, f"{filename}_{part_number:03d}.mp3")
             current_chunk.export(chunk_filename, format="mp3")
             part_number += 1
             current_chunk = AudioSegment.empty()
 
     # 마지막 남은 조각 저장
     if len(current_chunk) > 0:
-        chunk_filename = os.path.join(output_dir, f"part_{part_number}.mp3")
+        chunk_filename = os.path.join(output_dir, f"{filename}_{part_number:03d}.mp3")
         current_chunk.export(chunk_filename, format="mp3")
 
     print(f"파일이 {part_number}개의 부분으로 분할되었습니다. 출력 디렉토리: {output_dir}")
-
+    return part_number
 
 async def asplit_audio(file: UploadFile, min_silence_len=500, silence_thresh=-40, chunk_length=10000):
     # 업로드된 파일을 메모리에서 로드

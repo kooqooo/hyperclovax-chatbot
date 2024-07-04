8aadf7274156db485cce5f0f654e1c6bca644728
kooqooo <koogmlcks@gmail.com>
2024-07-04 15:27:11 +0900

    fix: 오디오 파일 업로드만 실행
    - 테스트 완료
    - 엔드포인트를 바꿀 필요가 있음
    
    #13

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 from datetime import datetime
 
+from dotenv import load_dotenv
+import torch
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
 import uvicorn
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,8 +16,22 @@ from backend.meetings import router as meeting_router
 from backend.meetings import Attendee, Meeting
 from backend.mongo_config import *
 from backend.ip_addresses import get_public_ip, get_private_ip
-from stt_inference import transcribe_audio, atranscribe_audio
+from stt_inference import transcribe_audio, atranscribe_audio, atranscribe_audio_with_model
 from audio_splitter import asplit_audio
+
+
+load_dotenv()
+model_name = os.getenv("MODEL_NAME")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+processor = WhisperProcessor.from_pretrained(model_name)
+model = WhisperForConditionalGeneration.from_pretrained(model_name).to(device)
+
+async def upload_to_gridfs(file: UploadFile, bucket: AsyncIOMotorGridFSBucket) -> str:
+    grid_in = bucket.open_upload_stream(file.filename)
+    file_content = await file.read()
+    await grid_in.write(file_content)
+    await grid_in.close()
+    return str(grid_in._id)
 
 app = FastAPI()
 app.include_router(meeting_router, prefix="/meetings", tags=["meetings"])
@@ -31,6 +48,11 @@ async def upload_file(file: UploadFile = File(...)):
     db = client[DATABASE_NAME]
     collection = db[COLLECTION_NAME]
     bucket = AsyncIOMotorGridFSBucket(db)
+    # GridFS에 파일 업로드
+    file_id = await upload_to_gridfs(file, bucket)
+    client.close()
+    return JSONResponse(status_code=200, content={"file_id": str(file_id)})
+    
     try:
         # 오디오 파일 분할
         split_files = await asplit_audio(file)
@@ -38,25 +60,17 @@ async def upload_file(file: UploadFile = File(...)):
         # STT
         transcriptions = []
         for split_file in split_files: # 로컬에 저장된 파일을 읽는게 더 빠름, 파일 하나마다 모델을 불러오는 듯?
-            transcription = await atranscribe_audio(split_file) # 비동기로 변경할 필요가 있음
+            transcription = await atranscribe_audio_with_model(split_file, model, processor, device) # 비동기로 변경할 필요가 있음
             transcriptions.append(transcription)
-        
-        # GridFS에 파일 업로드
-        grid_in = bucket.open_upload_stream(file.filename)
-        file_content = await file.read()
-        await grid_in.write(file_content)
-        await grid_in.close()
-        file_id = grid_in._id
         
         # 회의 정보 저장
         meeting = Meeting(
             title=file.filename,
-            audio_file_id=file_id, ####### <- 이 부분이 문제가 될 가능성이 있음
+            audio_file_id=str(file_id), ####### <- 이 부분이 문제가 될 가능성이 있음
             transcript="\n\n".join(transcriptions)
         )
-        await collection.insert_one(meeting)
-        
-        return JSONResponse(status_code=200, content={"file_id": str(file_id), "transcriptions": transcriptions})
+        await collection.insert_one(meeting.model_dump())
+        return JSONResponse(status_code=200, content={"file_id": str(file_id)})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:

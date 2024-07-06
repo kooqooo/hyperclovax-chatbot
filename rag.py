6c28df8230d1079378803636bea1be88429827cc
kooqooo <koogmlcks@gmail.com>
2024-07-07 01:25:12 +0900

    feat: 인스턴스 생성으로 클래스 호출 빈도 줄임 (수정 필요)
    #19

--- rag.py
+++ rag.py
@@ -4,7 +4,15 @@ from src.prompt_template import Prompts
 import os
 from dotenv import load_dotenv
 
-def chat_with_rag(query: str, executor: CompletionExecutor, k: int = 1) -> str:
+load_dotenv(override=True)
+API_KEY = os.getenv("API_KEY")
+API_KEY_PRIMARY_VAL = os.getenv("API_KEY_PRIMARY_VAL")
+REQUEST_ID = os.getenv("REQUEST_ID")
+TEST_APP_ID = os.getenv("TEST_APP_ID")
+
+executor = CompletionExecutor(API_KEY, API_KEY_PRIMARY_VAL, REQUEST_ID, TEST_APP_ID)
+
+def chat_with_rag(query: str, executor: CompletionExecutor = executor, k: int = 1) -> str:
     retrieval_result = faiss_inference(query, k)
     
     text = "- 너의 역할은 사용자의 질문에 reference를 바탕으로 답변하는거야. \n- 너가 가지고있는 지식은 모두 배제하고, 주어진 reference의 내용만을 바탕으로 답변해야해. \n- 만약 사용자의 질문이 reference와 관련이 없다면, {제가 가지고 있는 정보로는 답변할 수 없습니다.}라고만 반드시 말해야해."
@@ -19,13 +27,8 @@ def chat_with_rag(query: str, executor: CompletionExecutor, k: int = 1) -> str:
     return parse_response(result)
 
 def main(query: str, k: int =1):
-    load_dotenv(override=True)
-    API_KEY = os.getenv("API_KEY")
-    API_KEY_PRIMARY_VAL = os.getenv("API_KEY_PRIMARY_VAL")
-    REQUEST_ID = os.getenv("REQUEST_ID")
-    TEST_APP_ID = os.getenv("TEST_APP_ID")
-    
-    result = chat_with_rag(query, CompletionExecutor(API_KEY, API_KEY_PRIMARY_VAL, REQUEST_ID, TEST_APP_ID), k)
+
+    result = chat_with_rag(query, executor, k)
     print(result)
     
     return result

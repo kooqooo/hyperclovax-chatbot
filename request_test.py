3913cec4ceea3c937eaf6f6adeafc453cbf34a3e
kooqooo <koogmlcks@gmail.com>
2024-07-09 01:40:23 +0900

    fix: 변화된 main에 맞게 request 테스트 수정
    #23

--- request_test.py
+++ request_test.py
@@ -1,18 +1,10 @@
-""" Author: @maxseats
-서버에게 RAG 질의응답, FAISS CRUD 요청을 보내는 코드입니다.
-
-[API 요청]
-DB 초기화, RAG 질의응답, 회의록 PUT, 회의록 DELETE 순으로 요청을 보내는 코드에요.
-각 과정에 show_faiss_index()를 통해 DB의 상태를 확인할 수 있어요.
-필요에 따라 추후에 mongoDB와 연동하여 데이터를 저장하거나 삭제하는 로직을 추가할 수 있어요.
-"""
 import requests
-import urllib.parse
-from typing import List, Optional
 import json
 from datetime import datetime
 
-from vectordb_manager import show_faiss_index
+from backend.meeting_model import Meeting
+from utils.seoul_time import seoul_now, datetime_to_str, str_to_datetime
+
 
 # 서버 URL
 server_url = "http://127.0.0.1:8000/"
@@ -25,11 +17,11 @@ def get_answer(query: str) -> str:
     response = requests.get(url, params={"query": query})
     return response.json()
 
-def put_document(data_path, title: str = get_current_time(), created_date: str = get_current_time()):
-    url = server_url + 'document'
+def put_document(transcript: str, meeting_id: str, time: str = datetime_to_str(seoul_now())) -> dict:
+    url = server_url + 'documents'
     
     # 요청에 필요한 데이터
-    data = {"data_path": data_path, "title": title, "created_date": created_date}
+    data = {"transcript": transcript, "time": time, "meeting_id": meeting_id}
     
     # 요청 보내기
     headers = {"Content-Type": "application/json", "data": json.dumps(data)}
@@ -39,7 +31,7 @@ def put_document(data_path, title: str = get_current_time(), created_date: str =
     return response.json()
 
 def delete_document(doc_id: str):
-    url = server_url + 'document'
+    url = server_url + 'documents'
     headers = {"Content-Type": "application/json", "doc_id": doc_id}
     response = requests.delete(url, headers=headers)
     return response.json()
@@ -49,29 +41,102 @@ def delete_initialization():
     response = requests.delete(url)
     return response.json()
 
+def request_create_meeting(
+    file_id: str,
+    transcript: str,
+    time: str = datetime_to_str(seoul_now())
+) -> dict:
+    
+    url = server_url + 'meetings/'
+    time_datetime = str_to_datetime(time)
+    meeting = Meeting(
+        title=time,
+        transcript=transcript,
+        audio_file_id=file_id,
+        # faiss_file_id=id,
+        created_at=time_datetime
+    )
+    request_body = meeting.model_dump_json()
+    response = requests.post(url, data=request_body)
+    return response.json()
+
+def request_create_document(
+    meeting_id: str,
+    transcript: str,
+    time: str = datetime_to_str(seoul_now())
+) -> dict:
+    
+    url = server_url + 'documents'
+    
+    response = requests.post(url)
+    return response.json()
+
+def upload_audio_file(file_path: str) -> str:
+    url = server_url + 'files'
+    with open(file_path, 'rb') as file:
+        files = {'file': (file_path, file, 'application/octet-stream')}
+        response = requests.post(url, files=files)
+    return response.json()
+
+def request_segment_audio(uuid: str):
+    url = server_url + 'segment/' + uuid
+    response = requests.post(url)
+    return response.json()
+
+def request_stt(uuid: str):
+    url = server_url + 'stt/' + uuid
+    response = requests.get(url)
+    return response.json()
+
 
 if __name__ == "__main__":
+    import os
+    from config import PATH
     
-    # DB 초기화
-    delete_initialization()
-    print('1: '); show_faiss_index(); print()
+    # 오디오 파일 업로드
+    response = upload_audio_file(os.path.join(PATH, '디스코드 회의.mp3'))
+    uuid = response['uuid']
+    file_id = response['file_id']
+    print(f"Audio file uploaded with UUID: {uuid}, File ID: {file_id}.")
     
-    # RAG 질의응답 request
-    query = "파이썬을 어디에서 관리하는가?"
-    result = get_answer(query)
-    print(result, '\n')
+    # 오디오 파일 세그멘트
+    response = request_segment_audio(uuid)
+    num_files = response['num_files']
+    print(f"Audio file segmented into {num_files} files.")
+    
+    # STT 요청
+    response = request_stt(uuid)
+    transcript = response['transcript']
+    print(f"Transcript: {transcript}")
+    
+    now = seoul_now()
+    now_str = datetime_to_str(now)
 
-    '''
-    회의록 데이터 PUT request (DB에 저장)
-    회의록 txt파일 경로, title, created_date를 입력해줍니다.
-    title, created_date : 미 입력시 현재 시간으로 지정됩니다.
-    '''
-    put_document(data_path="./wiki_python.txt", title="Maxseats Test", created_date="2023-07-02 12:34:56")
-    put_document(data_path="./wiki_python.txt", title="Maxseats Test")
-    put_document(data_path="./wiki_python.txt")
-    print('2: '); show_faiss_index(); print()
+    # 회의기록 MongoDB에 업로드
+    response = request_create_meeting(file_id, transcript, now_str)
+    meeting = response
+    print(f"Meeting created meeting: {meeting}")
+    meeting_id = meeting['_id']
+    print(f"Meeting created with ID: {meeting_id}")
+
+    # FAISS 인덱스 초기화 or 추가
+    response = put_document(transcript, meeting_id, now_str)
+    print(f"Document created with response: {response}")
+
+    # 질문 1
+    query = "어떤 것을 이용해서 우리가 라벨링을 할 수 있을까요?" # 기대 답변: 저작도구
+    response = get_answer(query)
+    result = response['result']
+    print(f"Answer to '{query}': {result}")
     
-    # 회의록 DELETE test
-    # doc_id를 참조해서 Delete를 수행합니다.
-    delete_document('1')
-    print('1: '); show_faiss_index(); print()
+    # 질문 2
+    query = "라벨링을 위한 도구는 무엇인가요?" # 기대 답변: 저작도구
+    response = get_answer(query)
+    result = response['result']
+    print(f"Answer to '{query}': {result}")
+    
+    # 질문 3
+    query = "오늘 해야 할 일이 무엇인가요?" # 기대 답변: 위스파 모델 다 공부해 오는 거
+    response = get_answer(query)
+    result = response['result']
+    print(f"Answer to '{query}': {result}")

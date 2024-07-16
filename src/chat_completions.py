acb384187ad5c067714f2fbb8139a71599c468b8
구희찬 <koogmlcks@gmail.com>
2024-07-17 01:57:53 +0900

    chore: 주석 수정

--- src/chat_completions.py
+++ src/chat_completions.py
@@ -92,7 +92,7 @@ class RequestData:
         topP (float): 생성 토큰 후보군을 누적 확률을 기반으로 샘플링, 0 < topP <= 1 (기본값: 0.8)
         topK (int): 생성 토큰 후보군에서 확률이 높은 k개를 후보로 지정하여 샘플링, 0 <= topK <= 128 (기본값: 0)
         repeatPenalty (float): 같은 토큰을 생성하는 것에 대한 패널티 정도(설정값이 높을수록 같은 결괏값을 반복 생성할 확률 감소), 0 < repeatPenalty <= 10 (기본값: 5.0)
-        maxTokens (int): 생성 토큰 후보군에서 확률이 높은 k개를 후보로 지정하여 샘플링
+        maxTokens (int): 최대 생성 토큰 수, 0 <= maxTokens <= 4096 (기본값: 100)
         stopBefore (list[str]): 토큰 생성 중단 문자, (기본값: [])
         includeAiFilters (bool): 생성된 결괏값에 대해 욕설, 비하/차별/혐오, 성희롱 /음란 등 카테고리별로 해당하는 정도, (기본값: True)
         seed (int): 0일 때 일관성 수준이 랜덤 적용 (기본값: 0), 사용자 지정 seed 범위: 1 <= seed <= 4294967295
